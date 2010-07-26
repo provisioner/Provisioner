9cb1e8420389067ed7f86c81330c451563f91792
tm1000 <andrew.nagy@the159.com>
2010-07-25 23:27:45 -0700

    Added Aastra support for all models! WHOOOOOOO!

---
+++ endpoint/aastra/base.php
@@ -0,0 +1,13 @@
+<?PHP
+/**
+ * Aastra Base File
+ *
+ * @author Andrew Nagy
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_aastra_base extends endpoint_base {
+	
+	public $brand_name = 'aastra';
+	
+}
