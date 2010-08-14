866445893b905263264a97814d0ac92a122d1ee5
tm1000 <andrew.nagy@the159.com>
2010-08-14 12:00:26 -0700

    Added reboot commands for asterisk/unix. We can actually define different options for different operating systems and such however.

--- endpoint/snom/base.php
+++ endpoint/snom/base.php
@@ -8,4 +8,10 @@
  */
 class endpoint_snom_base extends endpoint_base {
 	public $brand_name = 'snom';
+	
+	function reboot() {
+		if(($this->engine == "asterisk") AND ($this->system == "unix")) {
+			exec("asterisk -rx 'sip notify reboot-snom ".$this->lines[1]['ext']."'");
+		}
+	}
 }
