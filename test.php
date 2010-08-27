67a125a7395cb8767faf07805905db15f27eedf8
dschreiber <d@d-man.org>
2010-08-27 01:53:15 -0700

    More work on JS-based endpoint manager

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <head>
         <!-- Begin JavaScript -->
         <script type="text/javascript" src="http://www.the159.com/test/jquery.js"></script>
-	<script src="javascript/endpoint_loader.js"></script>
+	<script src="javascript/endpoint.js"></script>
 	<link media="screen" rel="stylesheet" href="colorbox.css" />
 
     </head>
