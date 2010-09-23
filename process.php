d1f6aff717212041a431607f79cd93cadcc82f91
tm1000 <andrew.nagy@the159.com>
2010-09-23 00:44:49 -0700

    More fixes to process.php

--- process.php
+++ process.php
@@ -13,24 +13,26 @@ include('setup.php');
 // Allow running this test from the command line
 if (isset($_POST['brand'])) {
     $brand = $_POST['brand'];
-} elseif (isset($_REQUEST['brand'])) {
-	$brand = $_REQUEST['brand'];
+} elseif (isset($_REQUEST['brand_list'])) {
+        $brand = $_REQUEST['brand_list'];
 } else {
     $brand = $argv[1];
 }
 
 if (isset($_POST['family'])) {
     $family = $_POST['family'];
-} elseif (isset($_REQUEST['family'])) {
-	$family = $_REQUEST['family'];
+} elseif (isset($_REQUEST['model_list'])) {
+        $temp = explode('+',$_REQUEST['model_list']);
+		$family = $temp[0];
 } else {
     $family = $argv[2];
 }
 
 if (isset($_POST['model'])) {
     $model = $_POST['model'];
-} elseif (isset($_REQUEST['model'])) {
-	$model = $_REQUEST['model'];
+} elseif (isset($_REQUEST['model_list'])) {
+    $temp = explode('+',$_REQUEST['model_list']);
+	$model = $temp[1];
 } else {
     $model = $argv[3];
 }
@@ -71,7 +73,7 @@ $endpoint->lines[3] = array('ext' => '105', 'secret' => 'blah5', 'displayname' =
 //Set Variables according to the template_data files included. We can include different template.xml files within family_data.xml also one can create
 //template_data_custom.xml which will get included or template_data_<model_name>_custom.xml which will also get included
 //line 'global' will set variables that aren't line dependant
-$endpoint->options = 	array("admin_pass" =>  "password","main_icon" => "Main ICON Line #3");
+$endpoint->options =    array("admin_pass" =>  "password","main_icon" => "Main ICON Line #3");
 //Setting a line variable here...these aren't defined in the template_data.xml file yet. however they will still be parsed 
 //and if they have defaults assigned in a future template_data.xml or in the config file using pipes (|) those will be used, pipes take precedence
 
@@ -79,15 +81,14 @@ $endpoint->options = 	array("admin_pass" =>  "password","main_icon" => "Main ICO
 // Because every brand is an extension (eventually) of endpoint, you know this function will exist regardless of who it is
 $returned_data = $endpoint->generate_config();
 
-if((isset($_POST['brand'])) OR (isset($_REQUEST['brand']))) {
+if((isset($_POST['brand'])) OR (isset($_REQUEST['brand_list']))) {
     foreach($returned_data as $key => $files) {
         echo 'File:'.$key;
         if(in_array($key, $endpoint->protected_files)){
-        	echo " [<b>PROTECTED</b>]";
+                echo " [<b>PROTECTED</b>]";
         }
         echo '<br/><textarea rows="50" cols="100">'.$files.'</textarea><br/><br/>';
     }
 } else {
     print_r($returned_data);
 }
-
