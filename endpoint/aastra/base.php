3de5e417b9a0da9376250db6cda9d88195b99d8c
tm1000 <andrew.nagy@the159.com>
2010-09-24 00:45:37 -0700

    Adds Aastra Reboot support for Asterisk Systems.

--- endpoint/aastra/base.php
+++ endpoint/aastra/base.php
@@ -9,5 +9,11 @@
 class endpoint_aastra_base extends endpoint_base {
 	
 	public $brand_name = 'aastra';
+
+	function reboot() {
+		if(($this->engine == "asterisk") AND ($this->system == "unix")) {
+			exec("asterisk -rx 'sip notify aastra-check-cfg ".$this->lines[1]['ext']."'");
+		}
+	}
 	
 }
