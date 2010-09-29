2c90ec804323e6bd37b9b1b1e459573461fd7d7b
tm1000 <andrew.nagy@the159.com>
2010-09-29 00:16:14 -0700

    Updates to SPA phones

--- endpoint/cisco/spa/phone.php
+++ endpoint/cisco/spa/phone.php
@@ -13,11 +13,11 @@ class endpoint_cisco_spa_phone extends endpoint_cisco_base {
 	function generate_config() {
 		//spa likes lower case letters in its mac address
 		$this->mac = strtolower($this->mac);
-		$this->model = strtolower($this->model);
+		$temp_model = strtolower($this->model);
 		
 		//{$model}.cfg
 		$contents = $this->open_config_file("global.cfg");
-		$final[$this->model.'.cfg'] = $this->parse_config_file($contents, FALSE);
+		$final[$temp_model.'.cfg'] = $this->parse_config_file($contents, FALSE);
 				
 		//{$mac}.cfg
 		$contents = $this->open_config_file("\$mac.cfg");
