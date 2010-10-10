774abffdaad1df1c153e89857788da55776fc6a4
tm1000 <andrew.nagy@the159.com>
2010-10-09 23:38:50 -0700

    Yealink Reboot Commands (really just check-cfg)

--- endpoint/yealink/base.php
+++ endpoint/yealink/base.php
@@ -9,6 +9,12 @@
 abstract class endpoint_yealink_base extends endpoint_base {
 	
 	public $brand_name = 'yealink';
+
+	function reboot() {
+		if(($this->engine == "asterisk") AND ($this->system == "unix")) {
+			exec("asterisk -rx 'sip notify polycom-check-cfg ".$this->lines[1]['ext']."'");
+		}
+	}
 	
 }
 ?>
