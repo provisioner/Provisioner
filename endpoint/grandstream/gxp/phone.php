9ea181258fa4ef381c51e546f14f1dc869a3e87f
tm1000 <andrew.nagy@the159.com>
2011-01-05 01:04:50 -0800

    Updates to Grandstream, Yealink & Cisco

--- endpoint/grandstream/gxp/phone.php
+++ endpoint/grandstream/gxp/phone.php
@@ -16,7 +16,7 @@ class endpoint_grandstream_gxp_phone extends endpoint_grandstream_base {
 
                 // Grandstreams support lines 2-6, so let's add them if they're set
                 for ($i = 1; $i < 6; $i++) {
-                    $this->lines[$i]['line_active'] = (isset($this->lines[$i]['secret']) ? '1' : '0');
+                    $this->lines[$i]['options']['line_active'] = (isset($this->lines[$i]['secret']) ? '1' : '0');
                 }
 
 		$contents = $this->open_config_file("\$mac.cfg");
