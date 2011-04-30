08478d3d38184c1aca94bc832d5fc823e1ec7c86
Andrew Nagy <andrew.nagy@the159.com>
2011-04-29 22:05:14 -0700

    Adding support for Mitel 53xx models

---
+++ endpoint/mitel/base.php
@@ -0,0 +1,13 @@
+<?PHP
+/**
+ * Yealink Base File
+ *
+ * @author Andrew Nagy
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+abstract class endpoint_mitel_base extends endpoint_base {
+	
+	public $brand_name = 'mitel';	
+}
+?>
