cbfa09f8650824676ac8afdd667a4f4910d336ed
Andrew Nagy <andrew.nagy@the159.com>
2011-05-01 23:31:33 -0700

    A Bunch of Polycom Firmware Updates/Changes

--- endpoint/polycom/spipm/phone.php
+++ endpoint/polycom/spipm/phone.php
@@ -33,6 +33,9 @@ class endpoint_polycom_spipm_phone extends endpoint_polycom_base {
 			$file_list .= $this->mac.'_sip.cfg';
 			$final[$this->mac.'_sip.cfg'] = $this->parse_config_file($contents, FALSE);
 		} else {
+			$contents = $this->open_config_file('000000000000.cfg');
+			$final['000000000000.cfg'] = $this->parse_config_file($contents, FALSE);
+
 			$file_list .= ' sip.cfg';
 			$final['sip.cfg'] = $this->parse_config_file($contents, FALSE);
 			
