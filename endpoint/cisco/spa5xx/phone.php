eb9f5d7d508c639ac7881b2d8dd681578b9a4ccc
Andrew Nagy <andrew.nagy@the159.com>
2011-05-03 04:01:29 -0700

    All cisco SPA 5xxxx changes (I guess I skipped some before?)

--- endpoint/cisco/spa5xx/phone.php
+++ endpoint/cisco/spa5xx/phone.php
@@ -16,7 +16,34 @@ class endpoint_cisco_spa5xx_phone extends endpoint_cisco_base {
 		$temp_model = strtoupper($this->model);
 		$temp_model = str_replace("SPA", "spa", $temp_model);
 		
+		if(isset($this->options['lineops'])) {
+			foreach($this->options['lineops'] as $key => $data) {
+				$this->lines[$key]['options']['displaynameline'] = $data['displaynameline'];
+				$this->lines[$key]['options']['short_name'] = $data['displaynameline'];
+				if(($data['keytype'] == "blf") AND ($data['blfext'] != "")) {
+					$this->lines[$key]['ext'] = $data['blfext'];
+					$this->lines[$key]['secret'] = 'n/a';
+					$this->lines[$key]['options']['blf_ext_type'] = "Disabled";
+					$this->lines[$key]['options']['share_call_appearance'] = "shared";
+					$this->lines[$key]['options']['extended_function'] = "fnc=blf+sd+cp;sub=".$data['blfext']."@".$this->server[1]['ip'];
+				} else {
+					if(!isset($this->lines[$key]['secret'])) {
+						$this->lines[$key]['ext'] = '';
+						$this->lines[$key]['secret'] = '';
+						$this->lines[$key]['options']['blf_ext_type'] = "1";
+						$this->lines[$key]['options']['share_call_appearance'] = "private";
+						$this->lines[$key]['options']['extended_function'] = "";
+					} else {
+						$this->lines[$key]['options']['blf_ext_type'] = $key;
+						$this->lines[$key]['options']['share_call_appearance'] = "private";
+						$this->lines[$key]['options']['extended_function'] = "";
+					}
+				}
+			}
+		}
+		
 		//Yealink support lines 2-6, so let's add them if they're set
+		/***
         for ($i = 1; $i < 6; $i++) {
 			if(($this->lines[$i]['options']['line_key_type'] == "blf") AND ($this->lines[$i]['options']['blf_ext'] != "")) {
 				$this->lines[$i]['ext'] = $this->lines[$i]['options']['blf_ext'];
@@ -30,6 +57,7 @@ class endpoint_cisco_spa5xx_phone extends endpoint_cisco_base {
 				$this->lines[$i]['options']['extended_function'] = "fnc=blf+sd+cp;sub=".$this->lines[$i]['options']['blf_ext']."@".$this->server[1]['ip'];
 			} else {
 				$this->lines[$i]['options']['blf_ext_type'] = $i;
+				$this->lines[$i]['options']['display_name_line'] = $this->displayname;
 				//$this->lines[$i]['displayname'] = $this->lines[$i]['options']['display_name_line'];
 			}
 			$this->lines[$i]['options']['dial_plan'] = $this->options['dial_plan'];
@@ -37,6 +65,7 @@ class endpoint_cisco_spa5xx_phone extends endpoint_cisco_base {
 			$this->lines[$i]['options']['short_name'] = $this->lines[$i]['options']['display_name_line'];
 
 		}
+		**/
 		
 		//{$model}.cfg
 		$contents = $this->open_config_file("global.cfg");
