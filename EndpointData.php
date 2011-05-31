ceee063313a710c9fb0bd9f136fbbbe58385b04a
Andrew Nagy <andrew.nagy@the159.com>
2011-05-30 18:06:25 -0700

    Removing EndpointData.php & Adding a REAME File

--- EndpointData.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * This is totally cheating and either needs to be fixed or needs to be re-implemented properly
- */
-
-$xml = simplexml_load_file($_GET['filename']);
-$json = json_encode($xml);
-echo $json;
