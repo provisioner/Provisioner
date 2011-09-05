476393de2d70c6bfd6efc5f3d4c7b9ffa2ca3334
Jort Bloem <jort.bloem@btg.co.nz>
2011-09-06 09:55:27 +1200

    Convert the yealink phones to the new layout

--- endpoint/yealink/t2x/phone.php
+++ endpoint/yealink/t2x/phone.php
@@ -12,116 +12,75 @@ class endpoint_yealink_t2x_phone extends endpoint_yealink_base {
 
 	function parse_lines_hook($line) {
 		$this->lines[$line]['options']['line_active'] = (isset($this->lines[$line]['secret']) ? '1' : '0');
-        $this->lines[$line]['options']['line_m1'] = (isset($this->lines[$line]['secret']) ? $line-1 : '');
-        $this->lines[$line]['options']['voicemail_number'] = (isset($this->options['voicemail_number']) ? $this->options['voicemail_number'] : '');
+		$this->lines[$line]['options']['line_m1'] = (isset($this->lines[$line]['secret']) ? $line-1 : '');
+		$this->lines[$line]['options']['voicemail_number'] = (isset($this->options['voicemail_number']) ? $this->options['voicemail_number'] : '');
 		
 		if(isset($this->options['linekey'])) {
-			if (($line >= 1) && ($line <=6))
+			if (($line >= 1) && ($line <=6)) {
 				$this->options['linekey'][$line+10]=$this->options['linekey'][$line];
 			}
 		}
 	}
+	function prepare_for_generateconfig() {
+		# This contains the last 2 digits of y0000000000xx.cfg, for each model.
+		$model_suffixes=array('T28'=>'00','T26'=>'04','T22'=>'05','T20'=>'07');
+		//Yealink likes lower case letters in its mac address
+        	$this->mac = strtolower($this->mac);
+		$this->config_file_replacements['$suffix']=$model_suffixes[$this->model];
+		parent::prepare_for_generateconfig();
 
-    function generate_config() {
-        //Yealink likes lower case letters in its mac address
-        $this->mac = strtolower($this->mac);
-
-
-        //Global 0000000000000000-blah.cfg file (too many zeros man! why?)
-        $contents = $this->open_config_file("y000000000000.cfg");
-
-        //We go ahead and build this each time a phone is added/changed the other models have a different number at the end of the file name eg:-
-        //T28:y000000000000.cfg
-        //T26:y000000000004.cfg
-        //T22:y000000000005.cfg
-        //T20:y000000000007.cfg
-        switch($this->model) {
-            case "T28":
-                $final['y000000000000.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-            case "T26":
-                $final['y000000000004.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-            case "T22":
-                $final['y000000000005.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-            case "T20":
-                $final['y000000000007.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-        }
-		
 		if(isset($this->options['softkey'])) {
-            foreach($this->options['softkey'] as $key => $data) {
-                if ($this->options['softkey'][$key]['type'] == '0') {
-                    unset($this->options['softkey'][$key]);
+			foreach($this->options['softkey'] as $key => $data) {
+				if ($this->options['softkey'][$key]['type'] == '0') {
+					unset($this->options['softkey'][$key]);
 				}
 			}
 		}
 
 		if(isset($this->options['remotephonebook'])) {
-            foreach($this->options['remotephonebook'] as $key => $data) {
-                if ($this->options['remotephonebook'][$key]['url'] == '') {
-                    unset($this->options['remotephonebook'][$key]);
+			foreach($this->options['remotephonebook'] as $key => $data) {
+				if ($this->options['remotephonebook'][$key]['url'] == '') {
+					unset($this->options['remotephonebook'][$key]);
 				}
 			}
 		}
 	
-        if(isset($this->options['sdext38'])) {
-            foreach($this->options['sdext38'] as $key => $data) {
-                if ($this->options['sdext38'][$key]['type'] == '16') {
-                    $this->options['sdext38'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['sdext38'][$key]['value'];
-                } elseif ($this->options['sdext38'][$key]['type'] == '0') {
-                    unset($this->options['sdext38'][$key]);
-                } else {
-                    $this->options['sdext38'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
-
-        if(isset($this->options['memkey'])) {
-            foreach($this->options['memkey'] as $key => $data) {
-                if ($this->options['memkey'][$key]['type'] == '16') {
-                    $this->options['memkey'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey'][$key]['value'];
-                } elseif ($this->options['memkey'][$key]['type'] == '0') {
-                    unset($this->options['memkey'][$key]);
-                } else {
-                    $this->options['memkey'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
-
-        if(isset($this->options['memkey2'])) {
-            foreach($this->options['memkey2'] as $key => $data) {
-                if ($this->options['memkey2'][$key]['type'] == '16') {
-                    $this->options['memkey2'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey2'][$key]['value'];
-                } elseif ($this->options['memkey2'][$key]['type'] == '0') {
-                    unset($this->options['memkey2'][$key]);
-                } else {
-                    $this->options['memkey2'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
-
-        //$mac.cfg file
-        $contents = $this->open_config_file("\$mac.cfg");
-
-        $final[$this->mac.'.cfg'] = $this->parse_config_file($contents, FALSE);
-		
-		if($this->server_type == 'dynamic') {
-			$out = '';
-			$out[$this->mac.'.cfg'] = '';
-			foreach($final as $key => $value) {
-				$out[$this->mac.'.cfg'] .= $value . "\n";
-				if($key != $this->mac.'.cfg') {
-					$out[$key] = '#This File is intentionally left blank';
+		if(isset($this->options['sdext38'])) {
+			foreach($this->options['sdext38'] as $key => $data) {
+				if ($this->options['sdext38'][$key]['type'] == '16') {
+					$this->options['sdext38'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['sdext38'][$key]['value'];
+				} elseif ($this->options['sdext38'][$key]['type'] == '0') {
+					unset($this->options['sdext38'][$key]);
+				} else {
+					$this->options['sdext38'][$key]['pickup_value'] = '*8';
 				}
 			}
-			
-			return($out);
-		} else {
-			return($final);
 		}
 
-    }
+		if(isset($this->options['memkey'])) {
+			foreach($this->options['memkey'] as $key => $data) {
+				if ($this->options['memkey'][$key]['type'] == '16') {
+					$this->options['memkey'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey'][$key]['value'];
+				} elseif ($this->options['memkey'][$key]['type'] == '0') {
+					unset($this->options['memkey'][$key]);
+				} else {
+					$this->options['memkey'][$key]['pickup_value'] = '*8';
+				}
+			}
+		}
+
+		if(isset($this->options['memkey2'])) {
+			foreach($this->options['memkey2'] as $key => $data) {
+				if ($this->options['memkey2'][$key]['type'] == '16') {
+					$this->options['memkey2'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey2'][$key]['value'];
+				} elseif ($this->options['memkey2'][$key]['type'] == '0') {
+					unset($this->options['memkey2'][$key]);
+				} else {
+					$this->options['memkey2'][$key]['pickup_value'] = '*8';
+				}
+			}
+		}
+
+	}
 }
 ?>
--- endpoint/yealink/t3x/phone.php
+++ endpoint/yealink/t3x/phone.php
@@ -10,109 +10,80 @@ class endpoint_yealink_t3x_phone extends endpoint_yealink_base {
 
     public $family_line = 't3x';
 
-    function generate_config() {
-        //Yealink likes lower case letters in its mac address
-        $this->mac = strtolower($this->mac);
-
-
-        //Global 0000000000000000-blah.cfg file (too many zeros man! why?)
-        $contents = $this->open_config_file("y000000000000.cfg");
-
-        //We go ahead and build this each time a phone is added/changed the other models have a different number at the end of the file name eg:-
-        //T38:y000000000038.cfg
-        //T32:y000000000032.cfg
-        switch($this->model) {
-            case "T38":
-                $final['y000000000038.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-            case "T32":
-                $final['y000000000032.cfg'] = $this->parse_config_file($contents, FALSE);
-                break;
-        }
+	function prepare_for_generateconfig() {
+		# This contains the last 2 digits of y0000000000xx.cfg, for each model.
+		$model_suffixes=array('T38'=>'38','T32'=>'32');
+		//Yealink likes lower case letters in its mac address
+        	$this->mac = strtolower($this->mac);
+		$this->config_file_replacements['$suffix']=$model_suffixes[$this->model];
+		parent::prepare_for_generateconfig();
 
 		if(isset($this->options['linekey'])) {
 			foreach($this->options['linekey'] as $key => $data) {
-				if (($key >= 1) && ($key <=6))
+				if (($key >= 1) && ($key <=6)) {
 					$this->options['linekey'][$key+10]=$this->options['linekey'][$key];
 				}
 			}
 		}
 		
 		if(isset($this->options['softkey'])) {
-            foreach($this->options['softkey'] as $key => $data) {
-                if ($this->options['softkey'][$key]['type'] == '0') {
-                    unset($this->options['softkey'][$key]);
+			foreach($this->options['softkey'] as $key => $data) {
+				if ($this->options['softkey'][$key]['type'] == '0') {
+					unset($this->options['softkey'][$key]);
 				}
 			}
 		}
 
 		if(isset($this->options['remotephonebook'])) {
-            foreach($this->options['remotephonebook'] as $key => $data) {
-                if ($this->options['remotephonebook'][$key]['url'] == '') {
-                    unset($this->options['remotephonebook'][$key]);
+			foreach($this->options['remotephonebook'] as $key => $data) {
+				if ($this->options['remotephonebook'][$key]['url'] == '') {
+					unset($this->options['remotephonebook'][$key]);
 				}
 			}
 		}
 	
-        if(isset($this->options['sdext38'])) {
-            foreach($this->options['sdext38'] as $key => $data) {
-                if ($this->options['sdext38'][$key]['type'] == '16') {
-                    $this->options['sdext38'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['sdext38'][$key]['value'];
-                } elseif ($this->options['sdext38'][$key]['type'] == '0') {
-                    unset($this->options['sdext38'][$key]);
-                } else {
-                    $this->options['sdext38'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
+		if(isset($this->options['sdext38'])) {
+			foreach($this->options['sdext38'] as $key => $data) {
+				if ($this->options['sdext38'][$key]['type'] == '16') {
+					$this->options['sdext38'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['sdext38'][$key]['value'];
+				} elseif ($this->options['sdext38'][$key]['type'] == '0') {
+					unset($this->options['sdext38'][$key]);
+				} else {
+					$this->options['sdext38'][$key]['pickup_value'] = '*8';
+				}
+			}
+		}
 
-        if(isset($this->options['memkey'])) {
-            foreach($this->options['memkey'] as $key => $data) {
-                if ($this->options['memkey'][$key]['type'] == '16') {
-                    $this->options['memkey'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey'][$key]['value'];
-                } elseif ($this->options['memkey'][$key]['type'] == '0') {
-                    unset($this->options['memkey'][$key]);
-                } else {
-                    $this->options['memkey'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
+		if(isset($this->options['memkey'])) {
+			foreach($this->options['memkey'] as $key => $data) {
+				if ($this->options['memkey'][$key]['type'] == '16') {
+					$this->options['memkey'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey'][$key]['value'];
+				} elseif ($this->options['memkey'][$key]['type'] == '0') {
+					unset($this->options['memkey'][$key]);
+				} else {
+					$this->options['memkey'][$key]['pickup_value'] = '*8';
+				}
+			}
+		}
 
-        if(isset($this->options['memkey2'])) {
-            foreach($this->options['memkey2'] as $key => $data) {
-                if ($this->options['memkey2'][$key]['type'] == '16') {
-                    $this->options['memkey2'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey2'][$key]['value'];
-                } elseif ($this->options['memkey2'][$key]['type'] == '0') {
-                    unset($this->options['memkey2'][$key]);
-                } else {
-                    $this->options['memkey2'][$key]['pickup_value'] = '*8';
-                }
-            }
-        }
+		if(isset($this->options['memkey2'])) {
+			foreach($this->options['memkey2'] as $key => $data) {
+				if ($this->options['memkey2'][$key]['type'] == '16') {
+					$this->options['memkey2'][$key]['pickup_value'] = $this->options['call_pickup'].$this->options['memkey2'][$key]['value'];
+				} elseif ($this->options['memkey2'][$key]['type'] == '0') {
+					unset($this->options['memkey2'][$key]);
+				} else {
+					$this->options['memkey2'][$key]['pickup_value'] = '*8';
+				}
+			}
+		}
         
-        //Yealink support lines 2-6, so let's add them if they're set
-        for ($i = 1; $i < 6; $i++) {
-            $this->lines[$i]['options']['line_active'] = (isset($this->lines[$i]['secret']) ? '1' : '0');
-            $this->lines[$i]['options']['line_m1'] = (isset($this->lines[$i]['secret']) ? $i-1 : '');
-            $this->lines[$i]['options']['voicemail_number'] = (isset($this->options['voicemail_number']) ? $this->options['voicemail_number'] : '');
-        }
-
-
-
-
-        //$mac.cfg file
-        $contents = $this->open_config_file("\$mac.cfg");
-
-        $final[$this->mac.'.cfg'] = $this->parse_config_file($contents, FALSE);
-
-
-	//dialnow.xml file
-	$contents = $this->open_config_file("dialnow.xml");
-
-	$final['dialnow.xml'] = $this->parse_config_file($contents, FALSE);
-
-
-        return($final);
-    }
+		//Yealink support lines 2-6, so let's add them if they're set
+		for ($i = 1; $i < 6; $i++) {
+			$this->lines[$i]['options']['line_active'] = (isset($this->lines[$i]['secret']) ? '1' : '0');
+			$this->lines[$i]['options']['line_m1'] = (isset($this->lines[$i]['secret']) ? $i-1 : '');
+			$this->lines[$i]['options']['voicemail_number'] = (isset($this->options['voicemail_number']) ? $this->options['voicemail_number'] : '');
+		}
+	}
 }
 ?>
