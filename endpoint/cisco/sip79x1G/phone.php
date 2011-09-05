4a083a24a2f2bf72dba345e38ad8e65631f58e4a
Jort Bloem <jort.bloem@btg.co.nz>
2011-09-05 12:07:02 +1200

    Converted cisco to new per-file-compatible style generate_config (see change 325ed50e82767a5b06432eda9a325c90e7b6452d)

--- endpoint/cisco/sip79x1G/phone.php
+++ endpoint/cisco/sip79x1G/phone.php
@@ -9,34 +9,13 @@
 class endpoint_cisco_sip79x1G_phone extends endpoint_cisco_base {
 	
 	public $family_line = 'sip79x1G';
-	
-	function generate_config() {
-
+	function prepare_for_generateconfig() {
 		if(strlen($this->lines[1]['displayname']) > 12) {
 			$name = explode(" ", $this->lines[1]['displayname']);
 			$this->lines[1]['displayname'] = substr($name[0],0,12);
 		}
-		//Cisco likes lower case letters in its mac address
-		$this->mac = strtoupper($this->mac);
 		//Cisco time offset is in minutes, our global variable is in seconds
 		//$this->timezone = $global_cfg['gmtoff']/60;
-
-		//SEP{MAC}.cnf.xml
-		$contents = $this->open_config_file("SEP\$mac.cnf.xml");
-		$final['SEP'.$this->mac.'.cnf.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		//XMLDefault.cnf.xml
-		$contents = $this->open_config_file("XMLDefault.cnf.xml");
-		$final['XMLDefault.cnf.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		//RINGLIST.DAT
-		$contents = $this->open_config_file("RINGLIST.DAT");
-		$final['RINGLIST.DAT'] = $this->parse_config_file($contents, FALSE);
-		
-		//ringlist.xml
-		$contents = $this->open_config_file("ringlist.xml");
-		$final['ringlist.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		return($final);
 	}
+	
 }
--- endpoint/cisco/sip79xx/phone.php
+++ endpoint/cisco/sip79xx/phone.php
@@ -9,46 +9,13 @@
 class endpoint_cisco_sip79xx_phone extends endpoint_cisco_base {
 	
 	public $family_line = 'sip79xx';
-	
-	function generate_config() {
-
+	function prepare_for_generateconfig() {
+		parent::prepare_for_generateconfig();
 		if(strlen($this->lines[1]['displayname']) > 12) {
 			$name = explode(" ", $this->lines[1]['displayname']);
 			$this->lines[1]['displayname'] = substr($name[0],0,12);
 		}
-		//Cisco likes lower case letters in its mac address
-		$this->mac = strtoupper($this->mac);
 		//Cisco time offset is in minutes, our global variable is in seconds
 		//$this->timezone = $global_cfg['gmtoff']/60;
-
-		//SEP{MAC}.cnf.xml
-		$contents = $this->open_config_file("SEP\$mac.cnf.xml");
-		$final['SEP'.$this->mac.'.cnf.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		//SIP{MAC}.cnf
-		$contents = $this->open_config_file("SIP\$mac.cnf");
-		$final['SIP'.$this->mac.'.cnf'] = $this->parse_config_file($contents, FALSE);
-		
-		//SIPDefault.cnf
-		$contents = $this->open_config_file("SIPDefault.cnf");
-		$final['SIPDefault.cnf'] = $this->parse_config_file($contents, FALSE);
-		
-		//XMLDefault.cnf.xml
-		$contents = $this->open_config_file("XMLDefault.cnf.xml");
-		$final['XMLDefault.cnf.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		//RINGLIST.DAT
-		$contents = $this->open_config_file("RINGLIST.DAT");
-		$final['RINGLIST.DAT'] = $this->parse_config_file($contents, FALSE);
-		
-		//ringlist.xml
-		$contents = $this->open_config_file("ringlist.xml");
-		$final['ringlist.xml'] = $this->parse_config_file($contents, FALSE);
-		
-		//OS79XX.TXT
-		$contents = $this->open_config_file("OS79XX.TXT");
-		$final['OS79XX.TXT'] = $this->parse_config_file($contents, FALSE);
-		
-		return($final);
 	}
 }
