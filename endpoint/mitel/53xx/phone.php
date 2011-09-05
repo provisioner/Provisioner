e702184aa462a1ef25140e109177ea3ee667dadf
Jort Bloem <jort.bloem@btg.co.nz>
2011-09-05 14:13:22 +1200

    fix typo

--- endpoint/mitel/53xx/phone.php
+++ endpoint/mitel/53xx/phone.php
@@ -11,7 +11,7 @@ class endpoint_mitel_53xx_phone extends endpoint_mitel_base {
     public $family_line = '5xxx';
     public $dynamic_mapping = array(
 		'MN_$mac.cfg'=>array('MN_Generic.cfg','MN_$mac.cfg'),
-		'MN_Generic.cfg'=>'#This File is intentionally left blank';
+		'MN_Generic.cfg'=>'#This File is intentionally left blank',
     );
 
 }
