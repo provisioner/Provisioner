e99e8046a41bf332bc38abb954c265a45be030f8
Provisoner.net Web <andrew.nagy@the159.com>
2011-09-28 16:05:48 -0700

    Changes to rest generator for visual GUI

--- servers/rest/generate/generate.php
+++ servers/rest/generate/generate.php
@@ -23,20 +23,20 @@ $pest = new Pest('http://www.provisioner.net/r/v1/accounts');
 if(preg_match('/[0-9A-Fa-f]{12}/i', $file, $matches) && !(preg_match('/[0]{10}[0-9]{2}/i',$file))) {
 	$mac = $matches[0];
 	//Ironically we call our own rest here	
-	$data = $pest->get('/web/provision/'.strtoupper($mac));
+	$data = $pest->get('/'.$account_id.'/provision/'.strtoupper($mac));
 	$data = json_decode($data,TRUE);
 	
 	if(isset($data['data']['success']) && !$data['data']['success']) {
-		$data = $pest->get('/web/provision/'.strtolower($mac));
+		$data = $pest->get('/'.$account_id.'/provision/'.strtolower($mac));
 		$data = json_decode($data,TRUE);
 		if(isset($data['data']['success']) && !$data['data']['success']) {
 			die($data['data']['message']);
 		}
-	}
-			
-	$brand = $data['data']['statics']['brand'];
-	$family = $data['data']['statics']['family'];
-	$model = $data['data']['statics']['model'];
+	}		
+	
+	$brand = isset($data['phone_data']['brand']) ? $data['phone_data']['brand'] : $data['data']['statics']['brand'];
+	$family = isset($data['phone_data']['product']) ? $data['phone_data']['product'] : $data['data']['statics']['brand'];
+	$model = isset($data['phone_data']['model']) ? $data['phone_data']['model'] : $data['data']['statics']['brand'];
 
 	$class = "endpoint_" . $brand . "_" . $family . '_phone';
 
@@ -57,53 +57,61 @@ if(preg_match('/[0-9A-Fa-f]{12}/i', $file, $matches) && !(preg_match('/[0]{10}[0
 
 	//Phone Model (Please reference family_data.xml in the family directory for a list of recognized models)
 	$endpoint->model = $model;
-
-	//Timezone
-	if (!class_exists("DateTimeZone")) { require('tz.php'); }
-	$endpoint->DateTimeZone = new DateTimeZone($data['data']['statics']['timezone']);;
-
-	//Server IP Address & Port
-	$endpoint->server[1]['ip'] = $data['data']['statics']['server'];
-	$endpoint->server[1]['port'] = 5060;
 	
-	$endpoint->proxy[1]['ip'] = $data['data']['statics']['proxyserver'];
-	$endpoint->proxy[1]['port'] = 5060;
-	
+	//This is very very important
 	$endpoint->provisioning_path = 'http://www.provisioner.net'.dirname($_SERVER['REQUEST_URI'])."/";
 
-	//Provide alternate Configuration file instead of the one from the hard drive
-	//$endpoint->config_files_override['$mac.cfg'] = "{\$srvip}\n{\$admin_pass|0}\n{\$test.line.1}";
+	if(!isset($data['phone_data']['brand'])) {
+		//Timezone
+		if (!class_exists("DateTimeZone")) { require('tz.php'); }
+		$endpoint->DateTimeZone = new DateTimeZone($data['data']['statics']['timezone']);;
 
-	/*
-	foreach($data['data']['lines'] as $key => $data) {
-		$endpoint->lines[$key] = $data;
-	}
-	*/
+		//Server IP Address & Port
+		$endpoint->server[1]['ip'] = $data['data']['statics']['server'];
+		$endpoint->server[1]['port'] = 5060;
 	
-	$endpoint->lines = $data['data']['lines'];
-
-	/*
-	foreach($line_options as $key => $data) {
-		$endpoint->lines[$key]['options'] = $data['options'];
-	}
-
-	//Set Variables according to the template_data files included. We can include different template.xml files within family_data.xml also one can create
-	//template_data_custom.xml which will get included or template_data_<model_name>_custom.xml which will also get included
-	//line 'global' will set variables that aren't line dependant
-	*/
-	$endpoint->options =  $data['data']['options'];
+		$endpoint->proxy[1]['ip'] = $data['data']['statics']['proxyserver'];
+		$endpoint->proxy[1]['port'] = 5060;
 	
-	/*
-	//Setting a line variable here...these aren't defined in the template_data.xml file yet. however they will still be parsed 
-	//and if they have defaults assigned in a future template_data.xml or in the config file using pipes (|) those will be used, pipes take precedence
+		//Provide alternate Configuration file instead of the one from the hard drive
+		//$endpoint->config_files_override['$mac.cfg'] = "{\$srvip}\n{\$admin_pass|0}\n{\$test.line.1}";
 
-	*/
+		/*
+		foreach($data['data']['lines'] as $key => $data) {
+			$endpoint->lines[$key] = $data;
+		}
+		*/
+	
+		$endpoint->lines = $data['data']['lines'];
 
-	// Because every brand is an extension (eventually) of endpoint, you know this function will exist regardless of who it is
-	$returned_data = $endpoint->generate_config();
-	ksort($returned_data);
+		/*
+		foreach($line_options as $key => $data) {
+			$endpoint->lines[$key]['options'] = $data['options'];
+		}
 
-	echo $returned_data[$file];
+		//Set Variables according to the template_data files included. We can include different template.xml files within family_data.xml also one can create
+		//template_data_custom.xml which will get included or template_data_<model_name>_custom.xml which will also get included
+		//line 'global' will set variables that aren't line dependant
+		*/
+		$endpoint->options =  $data['data']['options'];
+	
+		/*
+		//Setting a line variable here...these aren't defined in the template_data.xml file yet. however they will still be parsed 
+		//and if they have defaults assigned in a future template_data.xml or in the config file using pipes (|) those will be used, pipes take precedence
+
+		*/
+
+		// Because every brand is an extension (eventually) of endpoint, you know this function will exist regardless of who it is
+		$returned_data = $endpoint->generate_config();
+		ksort($returned_data);
+
+		echo $returned_data[$file];
+	} else {
+		//rob junk
+		echo "rob junk";
+		print_r($data);
+		
+	}
 
 } elseif(preg_match('/[0]{10}[0-9]{2}/i',$file)) {
 	echo '#blank';
