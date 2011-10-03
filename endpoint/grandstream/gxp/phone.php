2c50da9fdaa93b72b3968e159fc3eabb012bacbd
Andrew Nagy <andrew.nagy@the159.com>
2011-10-03 13:52:18 -0700

    Adding Extension Boards for GXP Series

--- endpoint/grandstream/gxp/phone.php
+++ endpoint/grandstream/gxp/phone.php
@@ -16,6 +16,34 @@ class endpoint_grandstream_gxp_phone extends endpoint_grandstream_base {
                 for ($i = 1; $i < 6; $i++) {
                     $this->lines[$i]['options']['line_active'] = (isset($this->lines[$i]['secret']) ? '1' : '0');
                 }
+
+				if(isset($this->options['ext1'])) {
+					foreach($this->options['ext1'] as $key => $data) {
+						if ($this->options['ext1'][$key]['mode'] == '999') {
+							
+							$this->options['ext1'][$key]['account'] = '';
+							$this->options['ext1'][$key]['name'] = '';
+							$this->options['ext1'][$key]['uid'] = '';
+							$this->options['ext1'][$key]['mode'] = '';
+						}
+						
+						$this->options['ext1'][$key]['pnum'] = (strlen($key) == '1') ? '0'.$key : $key;
+					}
+				}
+				
+				if(isset($this->options['ext2'])) {
+					foreach($this->options['ext2'] as $key => $data) {
+						if ($this->options['ext2'][$key]['mode'] == '999') {
+							
+							$this->options['ext2'][$key]['account'] = '';
+							$this->options['ext2'][$key]['name'] = '';
+							$this->options['ext2'][$key]['uid'] = '';
+							$this->options['ext2'][$key]['mode'] = '';
+						}
+						
+						$this->options['ext2'][$key]['pnum'] = (strlen($key) == '1') ? '0'.$key : $key;
+					}
+				}
 	}
 	
 }
