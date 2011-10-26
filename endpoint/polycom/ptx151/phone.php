8f0519ff5b8f000ea04e9937084cbe4b87d7652e
Andrew Nagy <andrew.nagy@the159.com>
2011-10-26 10:07:44 -0700

    Final PTX151 fixes

--- endpoint/polycom/ptx151/phone.php
+++ endpoint/polycom/ptx151/phone.php
@@ -9,10 +9,13 @@
 class endpoint_polycom_ptx151_phone extends endpoint_polycom_base {
 
 	public $family_line = 'ptx151';	
-	public $copy_files = array("pg11gl3.bin","pg11sid.bin","pg11sid3.bin","pi110001.bin");
+	public $copy_files = array("pd11gl3.bin","pd11sid.bin","pd11sid3.bin","pi110001.bin");
 	
 	function config_files() {
-		$result=parent::config_files();       
+		$result=parent::config_files();
+		$ext = $this->lines[1]['ext'];
+		$result['sip_'.$ext.'.cfg'] = 'sip_$ext.cfg';
+		unset($result['sip_$ext.cfg']);    
 		return $result;
     }
 
