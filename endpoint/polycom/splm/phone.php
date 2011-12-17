73fcea1226f8b0661efb76e004e377f38129e4a5
Andrew Nagy <andrew.nagy@the159.com>
2011-12-16 22:47:02 -0800

    Fix up Polycom Legacy Models

--- endpoint/polycom/splm/phone.php
+++ endpoint/polycom/splm/phone.php
@@ -10,13 +10,46 @@
 class endpoint_polycom_splm_phone extends endpoint_polycom_base {
 
     public $family_line = 'splm';
+    private $configfiles = array();
     
-    function parse_lines_hook($line, $line_total) {
-        $this->settings['line'][$line]['digitmap'] = (isset($this->settings['digitmap']) ? $this->settings['digitmap'] : NULL);
-        $this->settings['line'][$line]['digitmaptimeout'] = (isset($this->settings['digitmaptimeout']) ? $this->settings['digitmaptimeout'] : NULL);
-        $this->settings['line'][$line]['microbrowser_main_home'] = (isset($this->settings['microbrowser_main_home']) ? $this->settings['microbrowser_main_home'] : NULL);
-        $this->settings['line'][$line]['idle_display'] = (isset($this->settings['idle_display']) ? $this->settings['idle_display'] : NULL);
-        $this->settings['line'][$line]['idle_display_refresh'] = (isset($this->settings['idle_display_refresh']) ? $this->settings['idle_display_refresh'] : NULL);
+    function parse_lines_hook($line_data, $line_total) {
+        $line_data['digitmap'] = (isset($this->settings['digitmap']) ? $this->settings['digitmap'] : NULL);
+        $line_data['digitmaptimeout'] = (isset($this->settings['digitmaptimeout']) ? $this->settings['digitmaptimeout'] : NULL);
+        return($line_data);
+    }
+    
+    function config_files() {
+        $result = parent::config_files();
+        $this->configfiles = array(
+            '$mac.cfg' => $this->mac . '_reg.cfg',
+            'server_317.cfg' => 'server_317.cfg',
+            'phone1_317.cfg' => 'phone1_317.cfg',
+            'sip_317.cfg' => 'sip_317.cfg'
+        );
+        
+        $macprefix = $this->server_type == 'dynamic' ? $this->mac . "_" : NULL;
+        if ((isset($this->settings['file_prefix'])) && ($this->settings['file_prefix'] != "")) {
+            $fp = $this->settings['file_prefix'];
+            foreach(array_values($this->configfiles) as $data) {
+                if(isset($result[$data]) AND $data != $this->mac . '_reg.cfg') {
+                    $result[$fp.$data] = $result[$data];
+                    $this->configfiles[$data] = $fp.$data;
+                    unset($result[$data]);
+                }
+            }
+        } elseif (isset($macprefix)) {
+            foreach(array_values($this->configfiles) as $data) {
+                if(isset($result[$data]) AND $data != $this->mac . '_reg.cfg') {
+                    $result[$macprefix.$data] = $result[$data];
+                    $this->configfiles[$data] = $macprefix.$data;
+                    unset($result[$data]);
+                }
+            }
+        }
+        
+        $this->settings['createdFiles'] = implode(', ', array_values($this->configfiles));
+        
+        return $result;
     }
 
     function prepare_for_generateconfig() {
@@ -24,7 +57,6 @@ class endpoint_polycom_splm_phone extends endpoint_polycom_base {
         $this->settings['mac'] = strtolower($this->mac);
         parent::prepare_for_generateconfig();
 
-        $this->settings['createdFiles'] = 'server_317.cfg, ' . $this->mac . '_reg.cfg, phone1_317.cfg, sip_317.cfg';
         $this->directory_structure = array("logs", "overrides", "contacts", "licenses");
         $this->copy_files = array("SoundPointIPLocalization", "SoundPointIPWelcome.wav");
         $this->protected_files = array('overrides/' . $this->mac . '-phone.cfg', 'logs/' . $this->mac . '-boot.log', 'logs/' . $this->mac . '-app.log', 'SoundPointIPLocalization');
