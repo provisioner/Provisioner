c7b483bc2e607868730450d41e65a1e08d9cd562
Jaybee- <jaybee@ego.gen.nz>
2012-01-16 09:03:42 +1300

    cisco sip79x1G: you can now specify the date format (US or EU)
    sip79x1G and sip79x1G: the name is limited to 12 characters. This now applies to all lines, not just the second [with index 1].

--- endpoint/cisco/sip79x1G/phone.php
+++ endpoint/cisco/sip79x1G/phone.php
@@ -12,9 +12,11 @@ class endpoint_cisco_sip79x1G_phone extends endpoint_cisco_base {
 	function prepare_for_generateconfig() {
 		parent::prepare_for_generateconfig();
 		$this->config_file_replacements['$mac']=strtoupper($this->mac);
-		if(strlen($this->lines[1]['displayname']) > 12) {
-			$name = explode(" ", $this->lines[1]['displayname']);
-			$this->lines[1]['displayname'] = substr($name[0],0,12);
+		foreach ($this->lines AS &$line) {
+			if (array_key_exists('displayname',$line) && (strlen($line['displayname']) > 12)) {
+				$name = explode(" ", $line['displayname']);
+				$line['displayname'] = substr($name[0],0,12);
+			}
 		}
 		//Cisco time offset is in minutes, our global variable is in seconds
 		//$this->timezone = $global_cfg['gmtoff']/60;
