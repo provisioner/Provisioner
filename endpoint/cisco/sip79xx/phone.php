8f73382b6d47546d3189611cd46500a225c45896
Andrew Nagy <andrew.nagy@the159.com>
2012-01-18 10:15:33 -0800

    Ok. Fixes Cisco 79xx. Shouldn't affect Jaybee I dont think

--- endpoint/cisco/sip79xx/phone.php
+++ endpoint/cisco/sip79xx/phone.php
@@ -9,13 +9,21 @@
 class endpoint_cisco_sip79xx_phone extends endpoint_cisco_base {
 	
 	public $family_line = 'sip79xx';
+	
+    function parse_lines_hook($line_data) {
+		if(strlen($line_data['displayname']) > 12) {
+			$name = explode(" ", $line_data['displayname']);
+			$line_data['shortname'] = substr($name[0],0,12);
+		} else {
+			$line_data['shortname'] = $line_data['displayname'];
+		}
+		return($line_data);
+	}
+	
 	function prepare_for_generateconfig() {
 		parent::prepare_for_generateconfig();
 		$this->config_file_replacements['$mac']=strtoupper($this->mac);
-		if(strlen($this->lines[1]['displayname']) > 12) {
-			$name = explode(" ", $this->lines[1]['displayname']);
-			$this->lines[1]['displayname'] = substr($name[0],0,12);
-		}
+
 		//Cisco time offset is in minutes, our global variable is in seconds
 		//$this->timezone = $global_cfg['gmtoff']/60;
 	}
