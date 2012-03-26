6e1ce287c2cfb0bc07e2f63f8be51f281c1a72c1
Andrew Nagy <andrew.nagy@the159.com>
2012-03-25 17:47:04 -0700

    Fixes tm1000/freepbx-endpointmanager#41

--- endpoint/grandstream/gxphd/phone.php
+++ endpoint/grandstream/gxphd/phone.php
@@ -30,20 +30,20 @@ class endpoint_grandstream_gxphd_phone extends endpoint_grandstream_base {
                 return($offset);
         }
 
-        function reboot() {
-                if(($this->engine == "asterisk") AND ($this->system == "unix")) {
-                        if(!isset($this->engine_location)) {
-                                $output = shell_exec("asterisk -rx 'sip show peers like ".$this->lines[1]['ext']."'");
-                        } else {
-                                $output = shell_exec($this->engine_location. " -rx 'sip show peers like ".$this->lines[1]['ext']."'");
-                        }
-                        if(preg_match("/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/",$output,$matches)) {
-                                $ip = $matches[0];
-                                $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
-                                //This is lame. I need to do this in php not over the command line. etc, I AM THE LAME.
-                                exec('curl -c cookies.txt -d"P2='.$pass.'&Login=Login&gnkey=0b82" http://'.$ip.'/cgi-bin/dologin');
-                                exec("curl -b cookies.txt http://".$ip."/cgi-bin/rs");
-                        }
-                }
-        }	
+	function reboot() {
+	        if(($this->engine == "asterisk") AND ($this->system == "unix")) {
+	                if(!isset($this->engine_location)) {
+	                        $output = shell_exec("asterisk -rx 'sip show peers like ".$this->settings['line'][0]['username']."'");
+	                } else {
+	                        $output = shell_exec($this->engine_location. " -rx 'sip show peers like ".$this->settings['line'][0]['username']."'");
+	                }
+	                if(preg_match("/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/",$output,$matches)) {
+	                        $ip = $matches[0];
+	                        $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
+	                        //This is lame. I need to do this in php not over the command line. etc, I AM THE LAME.
+	                        exec('curl -c cookies.txt -d"P2='.$pass.'&Login=Login&gnkey=0b82" http://'.$ip.'/cgi-bin/dologin');
+	                        exec("curl -b cookies.txt http://".$ip."/cgi-bin/rs");
+	                }
+	        }
+	}	
 }
