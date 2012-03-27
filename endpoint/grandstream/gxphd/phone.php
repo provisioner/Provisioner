7cfe681e85c8a1a417ed2a4eccbe8232512cf4c3
Andrew Nagy <andrew.nagy@the159.com>
2012-03-27 13:37:24 -0700

    Fixes issue #33 -Doing a simple str_replace as htmlentities didnt work

--- endpoint/grandstream/gxphd/phone.php
+++ endpoint/grandstream/gxphd/phone.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Grandstream GXP Phone File
  *
@@ -8,42 +9,48 @@
  */
 class endpoint_grandstream_gxphd_phone extends endpoint_grandstream_base {
 
-	public $family_line = 'gxphd';
+    public $family_line = 'gxphd';
 
-	function parse_lines_hook($line_data, $line_total) {
+    function parse_lines_hook($line_data, $line_total) {
         $line_data['line_active'] = (isset($line_data['secret']) ? '1' : '0');
         return($line_data);
     }
 
-	function get_gmtoffset($timezone) {
-		$timezone = str_replace(":", ".", $timezone);
-                $timezone = str_replace("30", "5", $timezone);
-                if(strrchr($timezone,'+')) {
-            		$num = explode("+",$timezone);
-                        $num = $num[1];
-                        $offset = 720 + ($num * 60);
-                } elseif(strrchr($timezone,'-')) {
-                        $num = explode("-",$timezone);
-                        $num = $num[1];
-                        $offset = 720 + ($num * -60);
-                }
-                return($offset);
+    function get_gmtoffset($timezone) {
+        $timezone = str_replace(":", ".", $timezone);
+        $timezone = str_replace("30", "5", $timezone);
+        if (strrchr($timezone, '+')) {
+            $num = explode("+", $timezone);
+            $num = $num[1];
+            $offset = 720 + ($num * 60);
+        } elseif (strrchr($timezone, '-')) {
+            $num = explode("-", $timezone);
+            $num = $num[1];
+            $offset = 720 + ($num * -60);
         }
+        return($offset);
+    }
+    
+    function prepare_for_generateconfig() {
+        parent::prepare_for_generateconfig();
+        $this->settings['dialplan'] = str_replace("+", "%2B", $this->settings['dialplan']);        
+    }
 
-	function reboot() {
-	        if(($this->engine == "asterisk") AND ($this->system == "unix")) {
-	                if(!isset($this->engine_location)) {
-	                        $output = shell_exec("asterisk -rx 'sip show peers like ".$this->settings['line'][0]['username']."'");
-	                } else {
-	                        $output = shell_exec($this->engine_location. " -rx 'sip show peers like ".$this->settings['line'][0]['username']."'");
-	                }
-	                if(preg_match("/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/",$output,$matches)) {
-	                        $ip = $matches[0];
-	                        $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
-	                        //This is lame. I need to do this in php not over the command line. etc, I AM THE LAME.
-	                        exec('curl -c cookies.txt -d"P2='.$pass.'&Login=Login&gnkey=0b82" http://'.$ip.'/cgi-bin/dologin');
-	                        exec("curl -b cookies.txt http://".$ip."/cgi-bin/rs");
-	                }
-	        }
-	}	
+    function reboot() {
+        if (($this->engine == "asterisk") AND ($this->system == "unix")) {
+            if (!isset($this->engine_location)) {
+                $output = shell_exec("asterisk -rx 'sip show peers like " . $this->settings['line'][0]['username'] . "'");
+            } else {
+                $output = shell_exec($this->engine_location . " -rx 'sip show peers like " . $this->settings['line'][0]['username'] . "'");
+            }
+            if (preg_match("/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/", $output, $matches)) {
+                $ip = $matches[0];
+                $pass = (isset($this->options['admin_pass']) ? $this->options['admin_pass'] : 'admin');
+                //This is lame. I need to do this in php not over the command line. etc, I AM THE LAME.
+                exec('curl -c cookies.txt -d"P2=' . $pass . '&Login=Login&gnkey=0b82" http://' . $ip . '/cgi-bin/dologin');
+                exec("curl -b cookies.txt http://" . $ip . "/cgi-bin/rs");
+            }
+        }
+    }
+
 }
