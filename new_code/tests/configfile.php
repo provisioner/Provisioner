bbfe504c075c052387ee764a10ce7dbe49ff2e53
Francis Genet <58907@supinfo.com>
2012-11-13 15:28:38 -0800

    Adding new_code

---
+++ new_code/tests/configfile.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+* Tests related to the config files generation for the provisioner
+*/
+class ConfigfileTest extends PHPUnit_Framework_TestCase {
+    public function testSetConfigFile() {
+        $this->assertFalse(true);
+    }
+}
+
+?>
