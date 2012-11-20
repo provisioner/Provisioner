783a0065f73f010b2d0414655cf5c30b3c9eb485
Francis Genet <58907@supinfo.com>
2012-11-19 17:40:09 -0800

    Adding php_on_couch + The bigcouch wrapper

--- new_code/index.php
+++ new_code/index.php
@@ -1,16 +1,54 @@
 <?php
 
+require_once 'BigCouch.php';
+
+$db = new BigCouch('http://localhost');
+
+echo '<pre>';
+print_r($db->loadSettings('test', 'afb4be6d5870518b8093d6ab290398dd'));
+echo '</pre>';
+
+exit();
+
+$db_type = "BigCouch";
+// We assume we have:
+// DATABASE: SYSTEM_ACCOUNT -- All global preferences/settings
+// DATABASE: PROVIDERS -- A document for each provider, by provider URL
+// DATABASE: <ACCOUNT_ID> - An account_id (which is random) which belongs to a provider and has all of a customer's default account settings AND the individual phone MAC address settings
+
+
 require_once '../bootstrap.php' ;
 require_once 'model/configfile.php';
 
 $mac = "00:15:65:00:00:00";
-$ua = "yealink SIP-T22P 7.40.1.2 00:15:65:00:00:00";
+$provider_id = "p.kazoo.io";
 
+// Retrieve settings about this MAC address
+$db = new $db_type($server_ip);
+
+$json_1 = $db->loadSettings("system_account", "globals");
+$json_2 = $db->loadSettings("providers", $provider_id);
+$json_3 = $db->loadSettings($account_id, "account_settings");
+$json_4 = $db->loadSEttings($account_id, $mac);
+
+if (!$final_settings['model'] or !$final_settings['brand']) {
+    // If model is unknown, try to auto-detect
+    $ua = "yealink SIP-T22P 7.40.1.2 00:15:65:00:00:00";    
+}
+
+// Prepare the template for this type of phone
 $test = new ConfigFile($mac, $ua);
+
+
+$test->import_settings($json);
 
 echo '<pre>';
 var_dump($test);
 echo '</pre>';
+
+
+
+
 
 /*$str_ua = $_SERVER['HTTP_USER_AGENT'];
 $str_requested_file = $_GET['file'];
