cc0bde614255a5216bf31dbc627737d5f985e337
Francis Genet <58907@supinfo.com>
2012-11-20 14:58:49 -0800

    organize bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,12 @@
 
 // Define default directories
 define("ROOT_PATH", dirname(__FILE__));
-define("MODULES_DIR", ROOT_PATH."/endpoint");
-define("PROVISIONER_BASE", ROOT_PATH.'/');
-define("LIB_BASE", PROVISIONER_BASE.'new_code/lib/');
+
+define("PROVISIONER_BASE", ROOT_PATH . '/');
+define("LIB_BASE", PROVISIONER_BASE . 'new_code/lib/');
+
+define("MODULES_DIR", ROOT_PATH . "/endpoint");
+define("WRAPPER_DIR", PROVISIONER_BASE . 'new_code/wrapper/');
 
 // Include our auto-loader
 require_once(PROVISIONER_BASE.'autoload.php');
