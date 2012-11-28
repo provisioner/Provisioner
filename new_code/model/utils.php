2ba0f3c29466eaf5bd38af7f45dc695f327b0dcd
Francis Genet <58907@supinfo.com>
2012-11-28 11:36:29 -0800

    Improving get_mac_address

--- new_code/model/utils.php
+++ new_code/model/utils.php
@@ -2,10 +2,17 @@
 
 class ProvisionerUtils {
     public static function get_mac_address($ua, $uri) {
+        // Let's check in the User-Agent
         if (preg_match("#[0-9a-fA-F]{2}(?=(:?))(?:\\1[0-9a-fA-F]{2}){5}#", $ua, $match_result))
-            return $match_result[0];
+            // need to return the mac address without the ':''
+            return preg_replace('/:/', '', $match_result[0]);
         else 
-            if (preg_match("#[0-9a-fA-F]{12}#", $uri, $match_result))
+            // Then let's check in the uri (should be at the end of it)
+            // Then explode the url
+            $explode_uri = explode('/', $uri);
+            $mac_index = sizeof($explode_uri) - 1;
+
+            if (preg_match("#[0-9a-fA-F]{12}#", $explode_uri[$mac_index], $match_result))
                 return $match_result[0];
             else 
                 return false;
@@ -13,8 +20,8 @@ class ProvisionerUtils {
 
     // Will return the host and only that
     // No 'www.' and no port
-    public static function get_provider($host_url) {
-        $host = preg_replace("/^www\./", '', $host_url);
+    public static function get_provider($http_host) {
+        $host = preg_replace("/^www\./", '', $http_host);
         $host = preg_replace("#:\d*$#", '', $host);
         return $host;
     }
