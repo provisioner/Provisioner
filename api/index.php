e181beaaf5d16ef75b32965f6059137bd1e2cef6
Francis Genet <58907@supinfo.com>
2012-12-12 15:53:17 -0800

    full implementation of the phones APIs - not tested

--- api/index.php
+++ api/index.php
@@ -5,9 +5,37 @@
 $db = new BigCouch('http://localhost');
 
 echo "<pre>";
-print_r($db->getAllByKey('factory_defaults', 'model', 't2x'));
-echo "</pre>";*/
+print_r($db->get('copy_defaults', 'yealink'));
+echo "</pre>";
 
+exit();
+
+delChildren($db->get('copy_defaults', 'yealink_t2x'));
+
+function delChildren($document) {
+    $db = new BigCouch('http://localhost');
+
+    if (array_key_exists('children', $document)) {
+        foreach ($document['children'] as $child) {
+            $doc_child = $db->get('copy_defaults', $child);
+            delChildren($doc_child);
+        }
+    }
+
+    if ($db->delete('copy_defaults', $document['_id'])) {
+        echo 'test';
+        echo "<br>";
+    }
+}*/
+
+/*require_once 'wrapper/bigcouch.php';
+
+$db = new BigCouch('http://localhost');
+
+echo "<pre>";
+print_r($db->getAllByKey('factory_defaults', 'model', 't2x'));
+echo "</pre>";
+*/
 require_once 'vendor/restler.php';
 use Luracast\Restler\Restler;
 
--- api/phones.php
+++ api/phones.php
@@ -9,6 +9,44 @@ class Phones {
         $this->db = new BigCouch('http://localhost');
     }
 
+    private function _buildDocumentName($brand, $family = null, $model = null) {
+        if ($model)
+            return '$brand_$family_$model';
+        elseif ($family)
+            return '$brand_$family';
+        elseif ($brand)
+            return '$brand';
+        else
+            return false;
+    }
+
+    private function _getParent($document_name) {
+        $elems = explode('_', $document_name);
+        if (sizeof($elems) == 1)
+            return false;
+        else {
+            array_pop($elems);
+
+            $parent_name = $elems[0];
+            for ($i=1; $i < sizeof($elems); $i++) { 
+                $parent_name = $parent_name . '_' . $elems[$i];
+            }
+
+            return $parent_name;
+        }
+    }
+
+    private function _delDocument() {
+        if (array_key_exists('children', $document)) {
+            foreach ($document['children'] as $child) {
+                $doc_child = $this->db->get('copy_defaults', $child);
+                $this->_delDocument($doc_child);
+            }
+        }
+
+        if ($this->db->delete('copy_defaults', $document['_id']));
+    }
+
     /**
      *  This is the function that will allow the administrator to retrieve all brands/families/models/
      *
@@ -41,19 +79,17 @@ class Phones {
      * @url POST /{brand}/{family}/{model}
      */
 
-    function editMethod($brand, $family = null, $model = null, $request_data = null) {
-        if ($model)
-            $document = '$brand_$family_$model';
-        elseif ($family)
-            $document = '$brand_$family';
-        elseif ($brand)
-            $document = '$brand';
-        else
+    function editSettings($brand, $family = null, $model = null, $request_data = null) {
+        if (empty($request_data))
+            throw new Exception(400, "The body for this request cannot be empty");
+
+        $document_name = buildDocumentName($brand, $family, $model);
+        if (!$document_name)
             throw new Exception(400, "Could not find at least the brand");
 
         foreach ($request_data as $key => $value) {
-            if ($this->db->update('factory_defaults', $document, $key, $value))
-                return array('status' => true, 'message' => 'Settings successfully modified');
+            if ($this->db->update('factory_defaults', $document_name, $key, $value))
+                return array('status' => true, 'message' => 'Document successfully modified');
             else
                 return array('status' => false, 'message' => 'Error while modifying the data');
         }
@@ -67,12 +103,37 @@ class Phones {
      * @url PUT /{brand}/{family}/{model}
      */
 
-    function put($brand, $family, $model, $request_data = null) {
+    function addDocument($brand, $family = null, $model = null, $request_data = null) {
+        if (empty($request_data))
+            throw new Exception(400, "The body for this request cannot be empty");
 
+        $document_name = buildDocumentName($brand, $family, $model);
+        if (!$document_name)
+            throw new Exception(400, "Could not find at least the brand");
+
+        $parent = $this->db->get('factory_defaults', $this->_getParent($document_name));
+        $this->db->update('factory_defaults', $document_name, 'children', array_push($parent['children'], $document_name));
+
+        $request_data = json_decode($request_data);
+        $request_data->_id = $document_name;
+
+        if ($this->db->add('factory_defaults', $request_data))
+            return array('status' => true, 'message' => 'Document successfully added');
+        else
+            return array('status' => false, 'message' => 'Error while adding the data');
     }
 
-    function delete($brand) {
-
+    /**
+     *  This is the function that will allow the administrator to delete a brand/family/model/
+     *
+     * @url DELETE /{brand}
+     * @url DELETE /{brand}/{family}
+     * @url DELETE /{brand}/{family}/{model}
+     */
+    function delete($brand, $family = null, $model = null) {
+        $document_name = buildDocumentName($brand, $family, $model);
+        if (!$document_name)
+            throw new Exception(400, "Could not find at least the brand");
     }
 }
 
