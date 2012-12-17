ade13357f743690c6f51efab12213b63b120e848
Francis Genet <58907@supinfo.com>
2012-12-17 11:02:29 -0800

    - comments

--- api/index.php
+++ api/index.php
@@ -1,42 +1,5 @@
 <?php
 
-/*require_once 'wrapper/bigcouch.php';
-
-$db = new BigCouch('http://localhost');
-
-echo "<pre>";
-print_r($db->get('copy_defaults', 'yealink'));
-echo "</pre>";
-
-exit();
-
-delChildren($db->get('copy_defaults', 'yealink_t2x'));
-
-function delChildren($document) {
-    $db = new BigCouch('http://localhost');
-
-    if (array_key_exists('children', $document)) {
-        foreach ($document['children'] as $child) {
-            $doc_child = $db->get('copy_defaults', $child);
-            delChildren($doc_child);
-        }
-    }
-
-    if ($db->delete('copy_defaults', $document['_id'])) {
-        echo 'test';
-        echo "<br>";
-    }
-}*/
-
-/*require_once 'wrapper/bigcouch.php';
-
-$db = new BigCouch('http://localhost');
-
-echo "<pre>";
-print_r($db->getAllByKey('factory_defaults', 'model', 't2x'));
-echo "</pre>";
-*/
-
 define(DB_SERVER, 'http://localhost');
 
 require_once 'wrapper/BigCouch.php';
