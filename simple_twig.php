95e1390c13fb8814a347ff848a4a1f758ec2e39a
Francis Genet <58907@supinfo.com>
2012-12-27 17:23:00 -0800

    Adding another class to twig_simple.php: Twig_Node_SetTemp

--- simple_twig.php
+++ simple_twig.php
@@ -7134,6 +7134,32 @@ class Twig_NodeVisitor_SafeAnalysis implements Twig_NodeVisitorInterface
     }
 }
 
+class Twig_Node_SetTemp extends Twig_Node
+{
+    public function __construct($name, $lineno)
+    {
+        parent::__construct(array(), array('name' => $name), $lineno);
+    }
+
+    public function compile(Twig_Compiler $compiler)
+    {
+        $name = $this->getAttribute('name');
+        $compiler
+            ->addDebugInfo($this)
+            ->write('if (isset($context[')
+            ->string($name)
+            ->raw('])) { $_')
+            ->raw($name)
+            ->raw('_ = $context[')
+            ->repr($name)
+            ->raw(']; } else { $_')
+            ->raw($name)
+            ->raw("_ = null; }\n")
+        ;
+    }
+}
+
+
 class Twig_Node_Expression_TempName extends Twig_Node_Expression
 {
     public function __construct($name, $lineno)
