1ede5d7025aefa5e39fa7de32755f9e33c8f7219
Francis Genet <58907@supinfo.com>
2013-01-15 17:36:59 -0800

    return from phone.php

--- endpoint/base.php
+++ endpoint/base.php
@@ -2,7 +2,7 @@
 
 abstract class endpoint_base {
     public function prepare($settings) {
-        
+        return $settings;
     }
 }
 
--- endpoint/yealink/t2x/phone.php
+++ endpoint/yealink/t2x/phone.php
@@ -3,6 +3,8 @@
 class endpoint_yealink_t2x_phone extends endpoint_yealink_base {
     function prepare($settings) {
         parent::prepare($settings);
+
+        return $settings;
     }
 }
 
