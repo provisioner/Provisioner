29803435cc5bf2ba8b636dbfa77e0d6823b6af23
Francis Genet <58907@supinfo.com>
2013-01-28 18:31:33 -0800

    Cisco abstract class reset

--- endpoint/cisco/ata18x/phone.php
+++ endpoint/cisco/ata18x/phone.php
@@ -7,20 +7,9 @@
  * @package Provisioner
  */
 class endpoint_cisco_ata18x_phone extends endpoint_cisco_base {
-	
-	public $family_line = 'ata18x';
+	function prepareConfig($settings) {
+        parent::prepareConfig($settings);
 
-    	function parse_lines_hook($line_data, $line_total) {
-		if(strlen($line_data['displayname']) > 12) {
-			$name = explode(" ", $line_data['displayname']);
-			$line_data['displayname'] = substr($name[0],0,12);
-		}
-        	return($line_data);
-    	}
-	
-	function prepare_for_generateconfig() {
-		parent::prepare_for_generateconfig();
-		//Cisco time offset is in minutes, our global variable is in seconds
-		//$this->timezone = $global_cfg['gmtoff']/60;
-	}
+        return $settings;
+    }
 }
