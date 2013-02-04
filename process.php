6c17c4b8eb7d29e93bf62b4600ba3bdd98ec13d5
Francis Genet <58907@supinfo.com>
2013-02-04 14:16:30 -0800

    Deleting comments + modifications for regex stuff

--- process.php
+++ process.php
@@ -4,6 +4,7 @@
  * Index file for the config generator
  *
  * @author Francis Genet
+ * @author Andrew Nagy
  * @license MPL / GPLv2 / LGPL
  * @package Provisioner
  * @version 5.0
@@ -25,33 +26,13 @@ if (!isset($argv)) {
     $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
     $http_host = strtolower($_SERVER['HTTP_HOST']);
 
-    // YEALINK
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/y000000000005.cfg";
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/0015652103df.cfg";
-    //$ua = strtolower("yealink SIP-T22P 7.61.0.80 00:15:65:21:03:df");
-
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/y000000000038.cfg";
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/00156527c764.cfg";
-    //$ua = strtolower("Yealink SIP-T38G  38.0.0.105 00:15:65:27:c7:64");
-
-    // Polycom
-    //$ua = strtolower("FileTransport PolycomSoundStationIP-SSIP_5000-UA/4.0.3.7562 (SN:0004f2e765da) Type/Application");
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/0004f2e765da_reg.cfg";
-
-    // Cisco
-    //$ua = "Cisco/SPA504G-7.4.9c (649EF3788E6A)(CCQ162306EA)";
-    //$uri = "/002e3a6fe532d90943e6fcaf08e1a408/spa504g.cfg";
-
     // Load the config manager
     // This will return a config_manager
     $config_manager_name = "ConfigGenerator_" . $settings->config_manager;
     $config_generator = new $config_manager_name();
-    $config_manager = $config_generator->get_config_manager($uri, $ua, $http_host, $settings);
 
-    // Set the file that will be generated
-    $target = ProvisionerUtils::strip_uri($uri);
+    $config_manager = $config_generator->get_config_manager($uri, $ua, $http_host, $settings);
     $config_manager->set_request_type('http');
-    $config_manager->set_config_file($target);
 
     echo $config_manager->generate_config_file();
 
