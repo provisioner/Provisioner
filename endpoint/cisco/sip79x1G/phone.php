a53cfcf80ad0e6f59745547312485a293fc3bc07
Francis Genet <58907@supinfo.com>
2013-02-13 11:21:33 -0800

    Adding regex for Cisco spa5xx

--- endpoint/cisco/sip79x1G/phone.php
+++ endpoint/cisco/sip79x1G/phone.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Cisco SIP 7900 Phone File
+ * Cisco SIP 79x1g Phone File
  *
  * @author Andrew Nagy
  * @license MPL / GPLv2 / LGPL
  * @package Provisioner
  */
-class endpoint_cisco_sip79x1G_phone extends endpoint_cisco_base {
+class endpoint_cisco_sip79x1g_phone extends endpoint_cisco_base {
 	function prepareConfig($settings, $config_manager) {
         parent::prepareConfig($settings, $config_manager);
 
