2c7635ab87c0461a657eadbdf3c08f52ebff0e08
Francis Genet <58907@supinfo.com>
2013-02-13 15:37:22 -0800

    Fix t3x abstract class

--- endpoint/yealink/t3x/phone.php
+++ endpoint/yealink/t3x/phone.php
@@ -9,8 +9,13 @@
  * @package Provisioner
  */
 class endpoint_yealink_t3x_phone extends endpoint_yealink_base {
+    public function __construct() {
+        parent::__construct();
+    }
+
     function prepareConfig($settings, $config_manager) {
         parent::prepareConfig($settings, $config_manager);
+        $this->encode_config($settings, $config_manager);
 
         return $settings;
     }
