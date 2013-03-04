3092e591635d97abce60818ffb49d604ef67357f
Andrew Nagy <andrew.nagy@the159.com>
2013-03-04 15:00:57 -0800

    Snom and merge and polycom stuff

---
+++ endpoint/snom/3xxEM/phone.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Snom 320, 360, 370 with Exp.Module Provisioning System
+ *
+ * @author Corrado Mella
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_snom_3xxEM_phone extends endpoint_snom_base {
+
+	public $family_line = '3xxEM';
+	
+}
