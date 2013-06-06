356c734f0fa7c6a11d615ec02a5b2593824c4e22
frifri <francis.genet@gmail.com>
2013-06-06 14:53:10 -0700

    Adding base files for Mocet 3xxx

---
+++ endpoint/mocet/3xxx/phone.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Mocet 3xxx Provisioning System
+ *
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_mocet_3xxx_phone extends endpoint_mocet_base {
+    public function __construct(&$config_manager) {
+        parent::__construct($config_manager);
+    }
+
+    function prepareConfig() {
+        parent::prepareConfig();
+    }
+}
+
+?>
