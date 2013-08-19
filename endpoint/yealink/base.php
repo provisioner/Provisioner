08dedcd9a4d77b87572243cac7de7ad8966f9d0c
frifri <francis.genet@gmail.com>
2013-08-19 11:00:19 -0700

    Indentation

--- endpoint/yealink/base.php
+++ endpoint/yealink/base.php
@@ -24,17 +24,17 @@ class endpoint_yealink_base extends endpoint_base {
 
         // Codecs
         if(isset($settings['media']['audio']['codecs'])) {
-	foreach ($settings['media']['audio']['codecs'] as $codec) {
-            if ($codec == "G729")
-                $settings['codecs']['g729'] = true;
-            elseif ($codec == "PCMU")
-                $settings['codecs']['pcmu'] = true;
-            elseif ($codec == "PCMA")
-                $settings['codecs']['pcma'] = true;
-            elseif ($codec == "G722_16" || $codec == "G722_32") 
-                $settings['codecs']['g722'] = true;
-        }
-	}
+            foreach ($settings['media']['audio']['codecs'] as $codec) {
+                if ($codec == "G729")
+                    $settings['codecs']['g729'] = true;
+                elseif ($codec == "PCMU")
+                    $settings['codecs']['pcmu'] = true;
+                elseif ($codec == "PCMA")
+                    $settings['codecs']['pcma'] = true;
+                elseif ($codec == "G722_16" || $codec == "G722_32") 
+                    $settings['codecs']['g722'] = true;
+            }
+	    }
 
         $this->config_manager->set_settings($settings);
     }
