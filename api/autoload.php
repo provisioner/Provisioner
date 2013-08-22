3ccd652af1947a52909dcf91a2e5836e84e033e6
frifri <francis.genet@gmail.com>
2013-08-21 17:00:04 -0700

    Adding bootstrap and autload for apis

---
+++ api/autoload.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * SPL Auto-loader
+ *
+ * @author Darren Schreiber
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class ProvisionerConfig {
+    /**
+     * Setup anything required to make our provisioner class work
+     */
+    public static function setup() {
+        // Register auto-loader. When classes are requested that aren't loaded
+        spl_autoload_register(array(
+            'ProvisionerConfig',
+            'autoload'
+        ));
+    }
+
+    public static function autoload($class) {		
+        // If for some reason we get here and the class is already loaded, return
+        if (class_exists($class, FALSE)) {
+            return TRUE;
+        }
+
+        // Try to include the class
+        $file = str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
+        if (is_file(PROVISIONER_BASE . $file)) {
+            require_once(PROVISIONER_BASE . $file);
+
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+}
+
+//Supress PHP output
+ob_start();
+ProvisionerConfig::setup();
+require_once LIB_BASE . 'simple_twig.php';
---
+++ api/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+// Define default directories
+define("ROOT_PATH", dirname(__FILE__));
+define("PROVISIONER_BASE", ROOT_PATH . '/');
+
+define("LIB_BASE", PROVISIONER_BASE . 'lib/');
+define("LOGS_BASE", PROVISIONER_BASE . 'logs/');
+define("MODULES_DIR", PROVISIONER_BASE . "endpoint/");
+
+// Include our auto-loader
+require_once(PROVISIONER_BASE . 'autoload.php');
