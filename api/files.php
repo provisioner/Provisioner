e151d79df7e740b28ccc1a5cb47310f4f2b96baf
frifri <francis.genet@gmail.com>
2013-08-21 17:00:20 -0700

    Adding files APIs

---
+++ api/files.php
@@ -0,0 +1,51 @@
+<?php 
+
+/**
+ * All methods in this class are protected - Some more than others
+ * Brand/family/model APIs
+ *
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ * @version 5.0
+ */
+
+require_once LIB_BASE . 'KLogger.php';
+
+class Files {
+    private $_settings;
+    private $_log;
+
+    function __construct() {
+        // First load the settings
+        $this->_settings = helper_settings::get_instance();
+
+        // Logger
+        $this->_log = KLogger::instance(LOGS_BASE, Klogger::DEBUG);
+    }
+
+    /**
+     *  This API will generate the config files for a given phone.
+     *  The idea is to merge the data from whatever datasource
+     *  and from the settings given to this API.
+     *  The settings sent through the APis will always override
+     *  the settings from the datasource.
+     *
+     * @url POST /generate
+     * @access protected
+     * @class  AccessControl {@requires admin}
+     */
+    function generate_files($request_data) {
+        $adapter_name = "adapter_" . $this->_settings->adapter . "_adapter";
+        $adapter = new $adapter_name();
+
+        $config_manager = $adapter->get_config_manager(
+            $request_data['provider_id'],
+            $request_data['mac'], 
+            $request_data['brand'], 
+            $request_data['model']
+        );
+
+        print_r($config_manager);
+    }
+}
