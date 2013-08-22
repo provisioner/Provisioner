731a0a74597c13d2321b1ceac17f8e03906dc3a2
frifri <francis.genet@gmail.com>
2013-08-21 17:00:49 -0700

    Updating index for files and users APIs

--- api/index.php
+++ api/index.php
@@ -9,9 +9,11 @@
  * @version 5.0
  */
 
-define('DB_SERVER', 'http://10.10.2.232');
-define('DB_PORT', '15984');
-define('DB_PREFIX', '');
+require_once 'bootstrap.php' ;
+require_once LIB_BASE . 'restler/restler.php';
+require_once LIB_BASE . 'KLogger.php';
+
+use Luracast\Restler\Restler;
 
 // CORS
 header('Access-Control-Allow-Headers:Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-Auth-Token');
@@ -19,18 +21,12 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Max-Age:86400');
 
-require_once 'wrapper/bigcouch.php';
-require_once 'utils_validator.php';
-require_once 'lib/restler/restler.php';
-require_once 'lib/KLogger.php';
-use Luracast\Restler\Restler;
-
 $r = new Restler();
 $r->setSupportedFormats('JsonFormat', 'UploadFormat');
 $r->addAPIClass('phones');
 $r->addAPIClass('providers');
 $r->addAPIClass('accounts');
+$r->addAPIClass('users');
+$r->addAPIClass('files');
 $r->addAuthenticationClass('AccessControl');
 $r->handle();
-
-?>
