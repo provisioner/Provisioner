c0f8f9c1ae832a2a298ce1abfca6436b27eda7c9
frifri <francis.genet@gmail.com>
2013-08-26 15:07:22 -0700

    Now set the directory url and provisioningurl in yealink base

--- api/endpoint/yealink/base.php
+++ api/endpoint/yealink/base.php
@@ -24,29 +24,37 @@ class endpoint_yealink_base extends endpoint_base {
 
         // Codecs
         if(isset($settings['media']['audio']['codecs'])) {
-			foreach ($settings['media']['audio']['codecs'] as $codec) {
-		            if ($codec == "G729")
-		                $settings['codecs']['g729'] = true;
-		            elseif ($codec == "PCMU")
-		                $settings['codecs']['pcmu'] = true;
-		            elseif ($codec == "PCMA")
-		                $settings['codecs']['pcma'] = true;
-		            elseif ($codec == "G722_16" || $codec == "G722_32") 
-		                $settings['codecs']['g722'] = true;
-		        }
-			}
+            foreach ($settings['media']['audio']['codecs'] as $codec) {
+	            if ($codec == "G729")
+	                $settings['codecs']['g729'] = true;
+	            elseif ($codec == "PCMU")
+	                $settings['codecs']['pcmu'] = true;
+	            elseif ($codec == "PCMA")
+	                $settings['codecs']['pcma'] = true;
+	            elseif ($codec == "G722_16" || $codec == "G722_32") 
+	                $settings['codecs']['g722'] = true;
+	        }
+		}
+
+        if ($this->config_manager->get_request_type() == 'http') {
+            $settings['provisioning_url'] = 'http://' . $this->config_manager->get_domain() . '/';
+            $settings['directory_url'] = $settings['provisioning_url'] . 'directory/' . $this->config_manager->get_mac_address() . '/phonebook1.xml';
+        }
+
         $this->config_manager->set_settings($settings);
     }
 	
 	public function setFilename($strFilename) {
 		$settings = $this->config_manager->get_settings();
-		
-		$model_suffixes = array('t28' => '00', 't26' => '04', 't22' => '05', 't20' => '07');
+		$constants = $this->config_manager->get_constants();
+
+        var_dump($this->config_manager->get_mac_address());
+
 		$model = $this->config_manager->get_model();
-		$strFilename = preg_replace('/\$suffix/', $model_suffixes[$model], $strFilename);
+		$strFilename = preg_replace('/\$suffix/', $constants['yealink']['suffixes'][$model], $strFilename);
 		
 		//Yealink likes lower case letters in its mac address
-		$strFilename = preg_replace('/\$mac/', strtolower($settings['mac']), $strFilename);
+		$strFilename = preg_replace('/\$mac/', strtolower($this->config_manager->get_mac_address()), $strFilename);
 		
 		return $strFilename;
 	}
