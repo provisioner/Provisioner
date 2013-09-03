77a34dd240516e611ace1a76599b00f14b4f69e9
frifri <francis.genet@gmail.com>
2013-09-03 16:21:36 -0700

    Some changes for ciscos and Polycoms

--- api/endpoint/cisco/base.php
+++ api/endpoint/cisco/base.php
@@ -17,7 +17,18 @@ class endpoint_cisco_base extends endpoint_base {
         parent::prepareConfig();
 
         $this->_set_timezone();
+        $this->_set_directory_url();
         $this->_set_codecs();
+    }
+
+    private function _set_directory_url() {
+        $constants = $this->config_manager->get_constants();
+        $settings = $this->config_manager->get_settings();
+
+        if ($this->config_manager->get_request_type() == 'http')
+            $settings['directory_url'] = $settings['provisioning_url'] . 'directory/' . $this->config_manager->get_mac_address() . '/directory.xml';
+        
+        $this->config_manager->set_settings($settings);
     }
 
     // Generating the timezone string
@@ -39,9 +50,6 @@ class endpoint_cisco_base extends endpoint_base {
         $right != null ? $final_tz = $final_tz . ':30' : $final_tz = $final_tz . ':00';
 
         $settings['timezone'] = $final_tz;
-
-        if ($this->config_manager->get_request_type() == 'http')
-            $settings['directory_url'] = $settings['provisioning_url'] . 'directory/' . $this->config_manager->get_mac_address() . '/directory.xml';
 
         $this->config_manager->set_settings($settings);
     }
