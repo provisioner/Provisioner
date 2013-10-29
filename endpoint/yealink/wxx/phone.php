bdfa658a2e7a8487f3def50b6ec51578de7735f4
frifri <francis.genet@gmail.com>
2013-10-29 14:55:02 -0700

    Adding support for yealink t4x and wxx

---
+++ endpoint/yealink/wxx/phone.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Yealink Wxx rules File
+ *
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_yealink_wxx_phone extends endpoint_yealink_base {
+    public function __construct(&$config_manager) {
+        parent::__construct($config_manager);
+    }
+
+    function prepareConfig() {
+        parent::prepareConfig();
+    }
+}
+
+?>
