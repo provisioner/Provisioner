5718771b0429900997aac8ddf611383219bfe3c8
frifri <francis.genet@gmail.com>
2013-10-31 12:46:49 -0700

    Adding t4x and wxx + tests

---
+++ endpoint/yealink/t4x/phone.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Yealink t3x rules File
+ *
+ * @author Andrew Nagy
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_yealink_t4x_phone extends endpoint_yealink_base {
+    public function __construct(&$config_manager) {
+        parent::__construct($config_manager);
+    }
+
+    function prepareConfig() {
+        parent::prepareConfig();
+    }
+}
+
+?>
