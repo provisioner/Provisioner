24fd9394799348dc6e47d704ae5fb2c45c837d13
Pedro Kiefer <pedro@kiefer.com.br>
2014-02-07 16:09:56 -0200

    Basic support for Intelbras TIP200 and TIP300 phones

---
+++ endpoint/intelbras/base.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Intelbras Base File
+ *
+ * @author Pedro Kiefer
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+abstract class endpoint_intelbras_base extends endpoint_base {
+
+    public $brand_name = 'intelbras';
+    protected $use_system_dst = FALSE;
+
+    function reboot() {
+        if (($this->engine == "asterisk") AND ($this->system == "unix")) {
+            exec($this->engine_location . " -rx 'sip notify reboot-intelbras " . $this->settings['line'][0]['username'] . "'");
+        }
+    }
+
+    function prepare_for_generateconfig() {
+        parent::prepare_for_generateconfig();
+        preg_match('/.*(-|\+)(\d*):(\d*)/i', $this->timezone['timezone'], $matches);
+        switch ($matches[3]) {
+            case '30':
+                $point = '.5';
+                break;
+            default:
+                $point = '';
+                break;
+        }
+        $this->timezone['timezone'] = $matches[1] . $matches[2] . $point;
+    }
+
+}
