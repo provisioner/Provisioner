411bd5a61e9bf96b268092d36610858188a34b4e
graeme <graeme@graeme-ThinkPad-Edge>
2014-07-18 17:18:34 +0200

    Added Tone data for extra support Fixed file generation on phone.php and base.php

--- endpoint/patton/base.php
+++ endpoint/patton/base.php
@@ -11,4 +11,8 @@ abstract class endpoint_patton_base extends endpoint_base {
 
     public $brand_name = 'patton';
 
+    function prepare_for_generateconfig() {
+        parent::prepare_for_generateconfig();
+        //$this->mac = strtoupper($this->mac);
+    }
 }
