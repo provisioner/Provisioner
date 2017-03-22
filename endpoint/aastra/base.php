9b1ba51f79699b1c8629818c3a6cb972f8760c4e
Richard Begg <badfish@meric.id.au>
2017-03-23 07:31:39 +1100

    pjsip_support: Add support for pjsip notifications
    In conjunction with the corresponding change to endpointman, use pjsip
    commands to send notifications where required

--- endpoint/aastra/base.php
+++ endpoint/aastra/base.php
@@ -14,7 +14,11 @@ class endpoint_aastra_base extends endpoint_base {
 
     function reboot() {
         if (($this->engine == "asterisk") AND ($this->system == "unix")) {
-            exec($this->engine_location . " -rx 'sip notify aastra-check-cfg " . $this->settings['line'][0]['username'] . "'");
+            if ($this->settings['line'][0]['tech'] == "pjsip") {
+                exec($this->engine_location . " -rx 'pjsip send notify aastra-check-cfg endpoint " . $this->settings['line'][0]['username'] . "'");
+            } else {
+                exec($this->engine_location . " -rx 'sip notify aastra-check-cfg " . $this->settings['line'][0]['username'] . "'");
+            }
         }
     }
 
