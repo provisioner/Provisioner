498e616d6924b9afc6cc038eb8d010251aa8600e
matthias1232 <matthias1232@users.noreply.github.com>
2018-05-15 16:31:44 +0200

    Add Snom M700 with Multicell Support and up to 150 Extensions

---
+++ endpoint/snom/m700/phone.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Snom 8xx Provisioning System
+ *
+ * @author Andrew Nagy & Jort
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_snom_m700_phone extends endpoint_snom_base {
+
+	public $family_line = 'm700';
+	
+	
+	
+}
